ed9e7c35736616c37d458a259ee5aa531a2b9a1f
divya prakash <d.pprakash1506@gmail.com>
2021-08-14 17:54:01 +0530

    added bubble sort

---
+++ src/BubbleSortImpl.java
@@ -0,0 +1,32 @@
+public class BubbleSortImpl {
+    public static void main(String[] args) {
+        int[] array = {20, 35, -15, 7, 55, 1, -22};
+        for(int lastunsortedIndex = array.length-1; lastunsortedIndex>0; lastunsortedIndex--){
+            for(int i=0; i<array.length-1;i++){
+                if(compare(array, i)){
+                    swap(array, i, i+1);
+                }
+            }
+        }
+
+        for(int n:array){
+            System.out.println(n);
+        }
+
+    }
+
+    public static void swap(int[] array, int i, int j){
+        if(i==j){
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    public static boolean compare(int[] array, int i){
+        return array[i] > array[i + 1];
+    }
+
+
+}
--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,20 @@
 public class Test {
+
+    public static void main(String[] args) {
+        int[] intArray = new int[7];
+
+        intArray[0] = 20;
+        intArray[1] = 38;
+        intArray[2] = 22;
+        intArray[3] = 24;
+        intArray[4] = 29;
+        intArray[5] = 28;
+        intArray[6] = 43;
+
+        for(int n:intArray){
+            System.out.println(n);
+        }
+    }
+
+
 }
