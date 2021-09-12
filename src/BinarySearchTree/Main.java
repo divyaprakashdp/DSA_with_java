976660a6fe74f3935dd7e44936ba1c5e2c203e76
divya prakash <d.prakash1506@gmail.com>
2021-09-12 12:29:27 +0530

    added BinarySearchTree

---
+++ src/BinarySearchTree/Main.java
@@ -0,0 +1,16 @@
+package BinarySearchTree;
+
+public class Main {
+    public static void main(String[] args) {
+        Tree intTree = new Tree();
+        intTree.insert(23);
+        intTree.insert(15);
+        intTree.insert(7);
+        intTree.insert(76);
+        intTree.insert(12);
+        intTree.insert(9);
+
+        intTree.traverseInOrder();
+
+    }
+}
