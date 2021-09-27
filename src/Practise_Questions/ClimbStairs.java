bd80d0dbe5c8e5ca75f2b5939aacd6c06ba4b514
divya prakash <d.prakash1506@gmail.com>
2021-09-27 19:49:30 +0530

    updated climbStairs(Question from leetcode)

--- src/Practise_Questions/ClimbStairs.java
+++ src/Practise_Questions/ClimbStairs.java
@@ -6,18 +6,24 @@ public class ClimbStairs {
         int n2 = 2;
         int n3 = 3;
 
-        System.out.println(climbStairs(n3));
+        System.out.println(climbStairs(11));
     }
 
     public static int climbStairs(int n){
 
-        if (n==1) return 1;
+        if (n<=1) return 1;
 
-        if(n%2==0){
-            return (n/2)+1;
-        }else{
-            return n/2;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i=3;i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
+
+        return dp[n];
+
     }
 }
 
