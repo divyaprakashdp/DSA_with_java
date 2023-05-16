988a87bc1c9b4a826aee2f6d87cfb72ecf445acf
divyaprakash <d.prakash1506@gmail.com>
2023-05-16 09:13:48 +0530

    added demoArrayList

--- src/RevisionCodes/JavaCollections.java
+++ src/RevisionCodes/JavaCollections.java
@@ -1,6 +1,9 @@
 package RevisionCodes;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class JavaCollections {
     public static void main(String[] args) {
@@ -10,16 +13,37 @@ public class JavaCollections {
     public static void demoArrayList(){
         ArrayList<Integer> aList = new ArrayList<>();
 
-        aList.add(12);
-        aList.add(22);
+        aList.add(52);
+        aList.add(72);
         aList.add(32);
         aList.add(42);
+        //Sorting ArrayList
+//        aList.sort(((o1, o2) -> o1.compareTo(o2)));
+        Collections.sort(aList);
+
 
         System.out.println(aList);
 
         aList.remove(0);
 
         System.out.println(aList);
+
+        //toArray
+        Object[] aListArray = aList.toArray();
+        for(Object i:aListArray){
+            System.out.println((Integer)i-2);
+        }
+
+        //Array to list
+        List<Object> newList= new ArrayList<>();
+        newList = Arrays.asList(aListArray);
+
+        System.out.println(newList);
+
+    }
+
+    public static  void demoLinkedList(){
+
     }
 
 }
